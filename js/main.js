2de3209f95b2c5f72082839da0c49b5521ccaed6
mdkoenig <mdkoenig@gmail.com>
2017-07-20 12:29:10 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "king", "queen", "king"];
+var cardsInPlay = [];
 
-console.log("User flipped " + cardThree);
-console.log("User flipped " + cardOne);
+var cardOne = cards[0];
+var cardTwo = cards[1];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log(cardsInPlay[0]);
+console.log(cardsInPlay[1]);
+
+var x = cardsInPlay.length;
+
+console.log(x);
+
+if (x === 2)
+{
+	if (cardsInPlay[0] === cardsInPlay[1])
+		{console.log("You found a match!");}
+	else 
+		{console.log("Sorry, try again");}
+}
+	else
+		{console.log("Pick again");}
